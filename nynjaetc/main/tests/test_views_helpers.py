ba46cb429c3e5d6d5777173b5cd2f7aad0817f03
Anders Pearson <anders@columbia.edu>
2013-07-15 15:24:24 -0400

    test coverage of already_visited_pages

--- nynjaetc/main/tests/test_views_helpers.py
+++ nynjaetc/main/tests/test_views_helpers.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AnonymousUser
 from django.test import TestCase
 from django.utils.timezone import utc
 from nynjaetc.main.models import SectionTimestamp
 from nynjaetc.main.views_helpers import whether_already_visited
+from nynjaetc.main.views_helpers import already_visited_pages
 from pagetree.models import Hierarchy
 
 
@@ -37,3 +38,36 @@ class WAVTest(TestCase):
             user=self.u,
             timestamp=datetime.utcnow().replace(tzinfo=utc), )
         self.assertTrue(whether_already_visited(self.section1, self.u))
+
+
+class AVPTest(TestCase):
+    def setUp(self):
+        self.u = User.objects.create(username="test1")
+        self.anon_u = AnonymousUser()
+        self.h = Hierarchy.objects.create(name="main", base_url="")
+        self.root = self.h.get_root()
+        self.root.add_child_section_from_dict(
+            {
+                'label': 'Section 1',
+                'slug': 'section-1',
+                'pageblocks': [],
+                'children': [],
+            })
+        r = self.root.get_children()
+        self.section1 = r[0]
+
+    def tearDown(self):
+        self.u.delete()
+        self.root.delete()
+        self.h.delete()
+
+    def test_already_visited_pages_none(self):
+        self.assertEquals(
+            already_visited_pages(self.section1, self.u),
+            [])
+
+    def test_already_visited_pages_anon(self):
+        """ calling with anon user returns the section """
+        self.assertEquals(
+            already_visited_pages(self.section1, self.anon_u),
+            self.section1)
