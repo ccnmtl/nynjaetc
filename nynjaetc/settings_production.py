be323df0784c2eafd00c5bd39880c962a6bbe0dd
Anders Pearson <anders@columbia.edu>
2014-02-05 16:59:30 -0500

    new sentry, new raven

--- nynjaetc/settings_production.py
+++ nynjaetc/settings_production.py
@@ -31,17 +31,7 @@ SENTRY_SERVERS = ['http://sentry.ccnmtl.columbia.edu/sentry/store/']
 ALLOWED_HOSTS = ['learn.nynjaetc.org']
 
 if 'migrate' not in sys.argv:
-    INSTALLED_APPS.append('raven.contrib.django')
-
-    import logging
-    from raven.contrib.django.handlers import SentryHandler
-    logger = logging.getLogger()
-    # ensure we havent already registered the handler
-    if SentryHandler not in map(type, logger.handlers):
-        logger.addHandler(SentryHandler())
-        logger = logging.getLogger('sentry.errors')
-        logger.propagate = False
-        logger.addHandler(logging.StreamHandler())
+    INSTALLED_APPS.append('raven.contrib.django.raven_compat')
 
 try:
     from local_settings import *
