6738797e111751617508a1bc1c4c27800f5acd26
Anders Pearson <anders@columbia.edu>
2014-05-19 16:09:31 -0400

    test to_json()

--- nynjaetc/treatment_activity/tests/test_models.py
+++ nynjaetc/treatment_activity/tests/test_models.py
@@ -7,6 +7,14 @@ class TreatmentNodeTest(TestCase):
         tn = TreatmentNodeFactory()
         self.assertEqual(str(tn), "  treatmentnode")
 
+    def test_json(self):
+        tn = TreatmentNodeFactory()
+        self.assertTrue('id' in tn.to_json().keys())
+        self.assertTrue('name' in tn.to_json().keys())
+        self.assertTrue('type' in tn.to_json().keys())
+        self.assertTrue('duration' in tn.to_json().keys())
+        self.assertTrue('help' in tn.to_json().keys())
+
 
 class TreatmentPathTest(TestCase):
     def test_unicode(self):
