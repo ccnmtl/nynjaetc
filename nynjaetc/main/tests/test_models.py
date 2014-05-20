26980bc57a13b99cf2e00b318ff0be6b7011a88f
Anders Pearson <anders@columbia.edu>
2014-05-20 15:34:55 -0400

    test some helper functions

--- nynjaetc/main/tests/test_models.py
+++ nynjaetc/main/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .factories import (UserFactory)
-from nynjaetc.main.models import UserProfile, next_with_content
+from nynjaetc.main.models import (
+    UserProfile, next_with_content, prev_with_content)
 
 
 class UserProfileTest(TestCase):
@@ -11,14 +12,42 @@ class UserProfileTest(TestCase):
 
 
 class DummySection(object):
+    def __init__(self, n=None, p=None, lohc=True):
+        self.next = n
+        self.prev = p
+        self.lohc = lohc
+
     def get_previous(self):
-        return None
+        return self.prev
 
     def get_next(self):
-        return None
+        return self.next
+
+    def is_leaf_or_has_content(self):
+        return self.lohc
 
 
 class HelpersTest(TestCase):
     def test_next_with_content_no_next(self):
         d = DummySection()
         self.assertEqual(next_with_content(d), None)
+
+    def test_next_with_content(self):
+        n = DummySection()
+        d = DummySection(n=n)
+        self.assertEqual(next_with_content(d), n)
+        d.lohc = False
+        d2 = DummySection(n=d)
+        self.assertEqual(next_with_content(d2), n)
+
+    def test_prev_with_content_no_prev(self):
+        d = DummySection()
+        self.assertEqual(prev_with_content(d), None)
+
+    def test_prev_with_content(self):
+        p = DummySection()
+        d = DummySection(p=p)
+        self.assertEqual(prev_with_content(d), p)
+        d.lohc = False
+        d2 = DummySection(p=d)
+        self.assertEqual(prev_with_content(d2), p)
