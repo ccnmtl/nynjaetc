774180e1e2ae4f4b11ddfac3bb69bfdc0942946d
Anders Pearson <anders@columbia.edu>
2014-05-20 15:25:40 -0400

    test UserProfile.__unicode__()

--- nynjaetc/main/tests/test_models.py
+++ nynjaetc/main/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from .factories import (UserFactory)
-from nynjaetc.main.models import UserProfile
+from nynjaetc.main.models import UserProfile, next_with_content
 
 
 class UserProfileTest(TestCase):
@@ -8,3 +8,17 @@ class UserProfileTest(TestCase):
         u = UserFactory()
         up, _created = UserProfile.objects.get_or_create(user=u)
         self.assertTrue(str(up).startswith("Encrypted email and"))
+
+
+class DummySection(object):
+    def get_previous(self):
+        return None
+
+    def get_next(self):
+        return None
+
+
+class HelpersTest(TestCase):
+    def test_next_with_content_no_next(self):
+        d = DummySection()
+        self.assertEqual(next_with_content(d), None)
