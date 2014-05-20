ccabce4cbae70f65fff6e5778ca519a2d555f156
Anders Pearson <anders@columbia.edu>
2014-05-20 13:58:33 -0400

    tests for choose_treatment_path()

--- nynjaetc/treatment_activity/tests/test_views.py
+++ nynjaetc/treatment_activity/tests/test_views.py
@@ -66,3 +66,42 @@ class GetNextStepsTest(TestCase):
         r = self.c.post("/_rgt/%d/%d/" % (tp.id, tn.id), dict(),
                         HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertNotEqual(r.status_code, 403)
+
+
+class ChooseTreatmentPathTest(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.u = User.objects.create(username="testuser")
+        self.u.set_password("test")
+        self.u.save()
+        self.c.login(username="testuser", password="test")
+
+    def tearDown(self):
+        self.u.delete()
+
+    def test_not_ajax(self):
+        r = self.c.post("/_rgt/")
+        self.assertEqual(r.status_code, 403)
+
+    def test_missing_params(self):
+        r = self.c.post("/_rgt/", dict(state='{}'),
+                        HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertNotEqual(r.status_code, 403)
+        self.assertTrue("Missing required" in r.content)
+
+    def test_missing_treatment_path(self):
+        r = self.c.post("/_rgt/",
+                        dict(state='{"cirrhosis":1,"status":1,"drug":1}'),
+                        HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(r.status_code, 200)
+        self.assertTrue("Can't find" in r.content)
+
+    def test_with_treatment_path(self):
+        tn = TreatmentNodeFactory()
+        TreatmentPathFactory(tree=tn, cirrhosis=1, treatment_status=1,
+                             drug_choice=1)
+        r = self.c.post("/_rgt/",
+                        dict(state='{"cirrhosis":1,"status":1,"drug":1}'),
+                        HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(r.status_code, 200)
+        self.assertFalse("Can't find" in r.content)
