688631d221753ca1596e95f5f6ed4d34d28f1d5e
Anders Pearson <anders@columbia.edu>
2014-05-30 15:00:52 -0400

    test coverage on one of the dreaded monkey patch methods

--- nynjaetc/main/tests/test_models.py
+++ nynjaetc/main/tests/test_models.py
@@ -4,7 +4,9 @@ from .factories import (
     SectionAlternateNavigationFactory,
 )
 from nynjaetc.main.models import (
-    UserProfile, next_with_content, prev_with_content)
+    UserProfile, next_with_content, prev_with_content,
+    my_quiz_submit,
+)
 
 
 class UserProfileTest(TestCase):
@@ -68,3 +70,31 @@ class SectionAlternateNavigationTest(TestCase):
         s = SectionFactory()
         san = SectionAlternateNavigationFactory(section=s)
         self.assertEqual(str(san), "Alternate nav for ")
+
+
+# first, we mock up a "Quiz" object
+class MockQuiz(object):
+    def original_submit(self, user, data):
+        self.user = user
+        self.data = data
+
+
+class MyQuizSubmitTest(TestCase):
+    def test_my_quiz_submit_basic(self):
+        d = MockQuiz()
+        u = UserFactory()
+        my_quiz_submit(d, u, dict())
+        self.assertEqual(d.user, u)
+        self.assertEqual(d.data, dict())
+
+    def test_my_quiz_submit_with_hrsa_id(self):
+        d = MockQuiz()
+        u = UserFactory()
+        # magic field is 'question17'
+        my_quiz_submit(d, u, dict(question17="foo"))
+        # it needs to scrub it from the data dictionary that it passes along
+        self.assertEqual(d.data, dict(question17="*****"))
+        # then it sets it on the user profile instead
+        the_profile, created = UserProfile.objects.get_or_create(user=u)
+        self.assertEqual(created, False)
+        self.assertEqual(the_profile.hrsa_id, "foo")
